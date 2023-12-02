754a56b420f90b4ad4df78d3063288f03ffda4df
BakerNet <hansbaker90@gmail.com>
2023-12-01 23:09:32 -0800

    cleaner code

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -14,18 +14,14 @@ pub fn part_one(input: &str) -> Option<u32> {
                 let game_id = game_re.captures(line).expect("Expected game_id to exist")[1]
                     .parse::<u32>()
                     .expect("Expect game_id to be an int");
-                let reds = red_re
-                    .captures_iter(line)
-                    .filter(|c| c[1].parse::<u32>().expect("Red capture should be int") > 12)
-                    .count();
-                let greens = green_re
-                    .captures_iter(line)
-                    .filter(|c| c[1].parse::<u32>().expect("Green capture should be int") > 13)
-                    .count();
-                let blues = blue_re
-                    .captures_iter(line)
-                    .filter(|c| c[1].parse::<u32>().expect("Blue capture should be int") > 14)
-                    .count();
+                let large_count = |re: Regex, max: u32| {
+                    re.captures_iter(line)
+                        .filter(|c| c[1].parse::<u32>().expect("Color capture should be int") > max)
+                        .count()
+                };
+                let reds = large_count(red_re, 12);
+                let greens = large_count(green_re, 13);
+                let blues = large_count(blue_re, 14);
                 if reds != 0 || greens != 0 || blues != 0 {
                     None
                 } else {
@@ -40,26 +36,20 @@ pub fn part_two(input: &str) -> Option<u32> {
     Some(
         input
             .lines()
-            .filter_map(|line| {
+            .map(|line| {
                 let red_re = Regex::new(r" (\d+) red").unwrap();
                 let green_re = Regex::new(r" (\d+) green").unwrap();
                 let blue_re = Regex::new(r" (\d+) blue").unwrap();
-                let reds = red_re
-                    .captures_iter(line)
-                    .map(|c| c[1].parse::<u32>().expect("Red capture should be int"))
-                    .reduce(|acc, e| if e > acc { e } else { acc })
-                    .unwrap();
-                let greens = green_re
-                    .captures_iter(line)
-                    .map(|c| c[1].parse::<u32>().expect("green capture should be int"))
-                    .reduce(|acc, e| if e > acc { e } else { acc })
-                    .unwrap();
-                let blues = blue_re
-                    .captures_iter(line)
-                    .map(|c| c[1].parse::<u32>().expect("blue capture should be int"))
-                    .reduce(|acc, e| if e > acc { e } else { acc })
-                    .unwrap();
-                Some(reds * greens * blues)
+                let largest_capture = |re: Regex| {
+                    re.captures_iter(line)
+                        .map(|c| c[1].parse::<u32>().expect("Color capture should be int"))
+                        .reduce(|acc, e| if e > acc { e } else { acc })
+                        .unwrap()
+                };
+                let reds = largest_capture(red_re);
+                let greens = largest_capture(green_re);
+                let blues = largest_capture(blue_re);
+                reds * greens * blues
             })
             .sum(),
     )
