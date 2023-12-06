43bb8c6cf7525554cd4454533957e66c43527998
BakerNet <hans@multimediallc.com>
2023-12-06 12:25:11 -0800

    refactor day 6 to use quadratic formula

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,30 +1,46 @@
 advent_of_code::solution!(6);
 
-fn extract_vec(s: &str) -> Vec<u32> {
+fn get_lower_race_quadratic(time: u64, record: u64) -> u64 {
+    let delta = time.pow(2) - 4 * record;
+    let delta_f64: f64 = delta as f64;
+    let delta_sq = delta_f64.sqrt();
+    let delta_sq_u64 = delta_sq as u64;
+    let root = (time - delta_sq_u64) / 2;
+    // usually root + 1, but sometimes off
+    let winning = if root * (time - root) > record {
+        root
+    } else {
+        root + 1
+    };
+    winning
+}
+
+fn extract_vec(s: &str) -> Vec<u64> {
     s.split(':')
         .last()
         .expect("There should be a :")
         .split_whitespace()
-        .map(|s| s.parse::<u32>().expect("Times should be numbers"))
+        .map(|s| s.parse::<u64>().expect("Times should be numbers"))
         .collect()
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let mut lines = input.lines();
-    let times: Vec<u32> = extract_vec(lines.next().expect("There should be a first line"));
-    let records: Vec<u32> = extract_vec(lines.next().expect("There should be a second line"));
+    let times: Vec<u64> = extract_vec(lines.next().expect("There should be a first line"));
+    let records: Vec<u64> = extract_vec(lines.next().expect("There should be a second line"));
     times
         .iter()
         .zip(records.iter())
         .map(|(&time, &record)| {
-            let mut x = 1;
-            // optimize with binary search instead linear search
-            let winning = loop {
-                if x * (time - x) > record {
-                    break x;
-                }
-                x += 1;
-            };
+            // naive approach - is actually slightly faster
+            // let mut x = 1;
+            // let winning = loop {
+            //     if x * (time - x) > record {
+            //         break x;
+            //     }
+            //     x += 1;
+            // };
+            let winning = get_lower_race_quadratic(time, record);
             time - winning - winning + 1
         })
         .reduce(|acc, x| acc * x)
@@ -44,14 +60,15 @@ pub fn part_two(input: &str) -> Option<u64> {
     let mut lines = input.lines();
     let time: u64 = extract_num(lines.next().expect("There should be a first line"));
     let record: u64 = extract_num(lines.next().expect("There should be a second line"));
-    let mut x = 1;
-    // optimize with binary search instead linear search
-    let winning = loop {
-        if x * (time - x) > record {
-            break x;
-        }
-        x += 1;
-    };
+    // naive approach - is much slower
+    // let mut x = 1;
+    // let winning = loop {
+    //     if x * (time - x) > record {
+    //         break x;
+    //     }
+    //     x += 1;
+    // };
+    let winning = get_lower_race_quadratic(time, record);
     Some(time - winning - winning + 1)
 }
 
